d089e9b19b21a8592f2e371e223801f439180f84
Mohamed Romdhani (INSAT) <mromdhani7@gmail.com>
2018-06-13 14:35:35 +0100

    Ajout de JWT coté client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { RouterModule} from '@angular/router';
 import { FormsModule} from '@angular/forms';
 
@@ -12,12 +12,18 @@ import { ComptesAddComponent } from './components/comptes-add/comptes-add.compon
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ComptesDeleteComponent } from './components/comptes-delete/comptes-delete.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { LogoutResolver } from './resolvers/logout-resolver';
 
 const mesRoutes = [
-  { path: 'list', component: ComptesListComponent},
+  { path: 'list', component: ComptesListComponent, canActivate: [AuthGuard]},
   { path: 'add', component: ComptesAddComponent},
   { path: 'welcome', component: WelcomeComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'delete/:id', component: ComptesDeleteComponent},
+  { path: 'logout', component: WelcomeComponent, resolve: [LogoutResolver] },
   { path: '', redirectTo: '/welcome', pathMatch: 'full'},
   { path: '**', component: NotFoundComponent},
 
@@ -32,7 +38,8 @@ const mesRoutes = [
     ComptesAddComponent,
     WelcomeComponent,
     NotFoundComponent,
-    ComptesDeleteComponent
+    ComptesDeleteComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +47,15 @@ const mesRoutes = [
     HttpClientModule,
     RouterModule.forRoot(mesRoutes)
   ],
-  providers: [],
+  providers: [
+    LogoutResolver,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../domain/user';
+import { AuthenticationService } from '../../services/authentication.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: User = new User();
+
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+
+  constructor(private authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    // reset login status
+            this.authenticationService.logout();
+            // get return url from route parameters or default to '/'
+            this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  onSubmit() {
+
+    this.loading = true;
+    this.authenticationService.login(this.user.username, this.user.password)
+        .pipe(first())
+        .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                this.error = error;
+                this.loading = false;
+            });
+
+  }
+
+}
