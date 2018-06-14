90e72e3a617fc15b77eb202b45f8de8fca30a7ab
Mohamed Romdhani (INSAT) <mromdhani7@gmail.com>
2018-06-14 14:36:27 +0100

    Adding JWT

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _servive: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  isLoggedIn(): boolean {
+    return this._servive.isLoggedIn();
+  }
+
+  getJwtSubjet(): string {
+    return this._servive.getJwtSubjet();
   }
 
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +16,40 @@ export class AuthenticationService {
             .pipe(map((res: any) => {
                 // login successful if there's a jwt token in the response
                 if (res && res.token) {
+
+
+                  // Debugging angular-jwt library
+                  // const helper = new JwtHelperService();
+                  // const decodedToken = helper.decodeToken(res.token);
+                  // console.log('****** JETON DECODE A la réception: ' + JSON.stringify(decodedToken));
+
                     // store username and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify({ username, token: res.token }));
-                }
+
+
+                  //   const content = localStorage.getItem('currentUser');
+                  //  const content_token_parsed = helper.decodeToken(JSON.parse(content).token);
+                  //   console.log('+++++++ JETON DECODE à partir du LOCAL STORAGE: ' + JSON.stringify (content_token_parsed));
+
+                  }
             }));
     }
 
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
+    }
+
+    isLoggedIn(): boolean {
+        return ( localStorage.getItem('currentUser')) ? true : false;
+    }
+
+    getJwtSubjet(): string {
+      const stored = localStorage.getItem('currentUser');
+      if (stored) {
+        const helper = new JwtHelperService();
+        return helper.decodeToken(JSON.parse(stored).token).sub;
+      }
+      return null;
     }
 }
